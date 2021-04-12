054927114516ebd1668d8bc299ec75148dd4fe29
Jason <jasonsinghdeol@gmail.com>
2021-04-12 00:18:12 -0700

    Eugenes additions. Implemented save and load

--- Game.pl
+++ Game.pl
@@ -1,79 +1,72 @@
+:- style_check(-singleton).
 :- dynamic current_node_is/1, equipped/2, located/2, health/2, defense/2, attack/2, magic_attack/2, magic_defense/2, prev_node/1, status/2,
-    king_status/2, gold/1.
+    king_status/2, gold/1, potion_count/2, interactable/2.
 
 different(X, X) :- !, fail.
 different(X, Y).
 
-/* Ideas:
-    -To save or load, the only informaiton we need is the location and equipment.
-    -This is due to the level checks being based on equipment only.
-    -Therefore our save files can just be text files, that we parse.
-
-    -Have battle scenarios where we clear the terminal and show both
-    -our health points and the enemy health points.
-    -We can choose an attack from a menu and then calculate damage
-    -and refresh the terminal with updated helth values, repeat for enemy.
-*/
-
-/* The players current location. */
-current_node_is(home).
-
 /*
 The players equipment.
 */
+load_default_equipment :-
+    assert(equipped(weapon_slot, nothing)),
+    assert(equipped(armor_slot, nothing)),
+    assert(equipped(head_slot, nothing)),
+    assert(equipped(magic_slot, nothing)),
+    assert(potion_count(health_potion, 0)),
+    assert(gold(0)).
 
-equipped(weapon_slot, nothing).
-equipped(armor_slot, nothing).
-equipped(head_slot, nothing).
-equipped(magic_slot, nothing).
-
-gold(0).
+add_gold(I):- gold(X), NG is X + I, assert(gold(NG)), retract(gold(X)), format(" gold increased to : ~w", [NG]), nl.
 
 /*
 stats for characters
 */
+
+load_default_player_stats :-
+    assert(health(player, 50)),
+    assert(defense(player, 1)),
+    assert(attack(player, 1)),
+    assert(magic_attack(player, 1)),
+    assert(magic_defense(player, 4)).
+
 king_status(player, no).
 health(slime, 10).
 health(ghost, 10).
-health(player, 50).
 health(king, 50).
 health(troll, 20).
 health(skeleton, 5).
 
 defense(slime, 1).
 defense(ghost, 100).
-defense(player, 1).
 defense(king, 5).
 defense(troll, 2).
 defense(skeleton, 4).
 
 attack(slime, 1).
 attack(ghost, 0).
-attack(player, 1).
 attack(king, 7).
 attack(troll, 3).
 attack(skeleton, 1).
 
 magic_attack(slime, 1).
 magic_attack(ghost, 2).
-magic_attack(player, 1).
 magic_attack(king, 5).
 magic_attack(troll, 0).
 magic_attack(skeleton, 4).
 
 magic_defense(slime, 1).
 magic_defense(ghost, 1).
-magic_defense(player, 4).
 magic_defense(king, 5).
 magic_defense(troll, 1).
 magic_defense(skeleton, 4).
 
-status(slime, alive).
-status(ghost, alive).
-status(player, alive).
-status(king, alive).
-status(troll, alive).
-status(skeleton, alive).
+load_default_status :-
+    assert(status(slime, alive)),
+    assert(status(ghost, alive)),
+    assert(status(player, alive)),
+    assert(status(king, alive)),
+    assert(status(troll, alive)),
+    assert(status(skeleton, alive)).
 
 /*
 damage calculations
@@ -109,7 +102,7 @@ check_dead(player):-  health(player, H), H < 1 , status(player, S), assert(statu
  nl, write("You are dead. Game Over."), nl, halt(1).
 
 check_dead(X):- different(X, player), different(X, king), health(X, H), H < 1 , status(X, S), assert(status(X, dead)), retract(status(X, S)),
- nl, write(X), write(" is dead. You are safe to travel again."), nl, current_node_is(N), prev_node(PN),
+ nl, write(X), write(" is dead. You are safe to travel again."), nl, current_node_is(N), prev_node(PN), dead_event(X),
  assert(current_node_is(PN)), retract(current_node_is(N)), retract(prev_node(PN)), !.
  check_dead(_) :- !.
 
@@ -117,36 +110,37 @@ check_dead(X):- assert(X, king), health(X, H), H < 1, status(X, S), assert(statu
  nl, write("The king is finally dead. Now go home and restore balance to your homeland!"), nl, current_node_is(N), prev_node(PN), % Aiden
  assert(current_node_is(PN)), retract(current_node_is(N)), retract(prev_node(PN)), !.
 
-/*
-events for each location.
-*/
-event(slime_field):-
- current_node_is(PN), status(slime, alive), assert(prev_node(PN)), different(battle_dimension, PN), assert(current_node_is(battle_dimension)), retract(current_node_is(PN)),
- write("You are attacked by a slime!"), describe_battle(player, slime), !. 
+dead_event(slime):- add_potion(health_potion), add_gold(10).
+dead_event(_).
 
-event(cemetery2):-
- current_node_is(PN), status(ghost, alive), assert(prev_node(PN)), different(battle_dimension, PN), assert(current_node_is(battle_dimension)), retract(current_node_is(PN)),
- write("As you come upon a particular gravestone, a ghost emerges from the fog and attacks you!"), describe_battle(player, ghost), !.
-
-event(river) :-
- current_node_is(PN), status(troll, alive), assert(prev_node(PN)), different(battle_dimension, PN), assert(current_node_is(battle_dimension)), retract(current_node_is(PN)),
- write("You come to a bridge that spans the river, but a troll jumps out to block your way!"), describe_battle(player, troll), !.
-
-event(tower) :-
- current_node_is(PN), status(skeleton, alive), assert(prev_node(PN)), different(battle_dimension, PN), assert(current_node_is(battle_dimension)), retract(current_node_is(PN)),
- write("After climbing the spiral staircase to the top of the tower, the skeletal remains of an ancient wizard attacks you!"), describe_battle(player, skeleton), !. 
-
-event(boss):-
- current_node_is(PN), status(king, alive), assert(prev_node(PN)), different(battle_dimension, PN), assert(current_node_is(battle_dimension)), retract(current_node_is(PN)),
- write("You enter the throne room; the evil king is waiting with his broadsword at the ready. He is large and cunning, you will need all your strength to defeat him."), nl,
- write("You lunge forward to engage the evil king in battle!"), nl, describe_battle(player, king), !.
-
-event(_) :- !.
+battle(X):- current_node_is(PN), status(X, alive), assert(prev_node(PN)), different(battle_dimension, PN),
+ assert(current_node_is(battle_dimension)), retract(current_node_is(PN)), describe_battle(player, X).
 
 describe_battle(X, Y):- health(X, H1), health(Y, H2), nl, 
     format("You have ~0f health remaining.", [H1]), nl,
     format("~w has ~0f health remaining.", [Y, H2]), nl,
     write("your options are attack("), write(Y), write(") and magic_attack("), write(Y), write(")"), nl, !.
+/*
+events for each location.
+*/
+
+event(slime_field):- 
+status(slime, alive), write("You are attacked by a slime!"), battle(slime), !. 
+
+event(river) :-
+status(troll, alive), write("You come to a bridge that spans the river, but a troll jumps out to block your way!"), battle(troll), !.
+
+event(tower) :-
+status(skeletan, alive), write("After climbing the spiral staircase to the top of the tower, the skeletal remains of an ancient wizard attacks you!"), battle(skeleton), !. 
+
+event(boss):-
+status(king, alive), write("You enter the throne room; the evil king is waiting with his broadsword at the ready. He is large and cunning, you will need all your strength to defeat him."), nl,
+ write("You lunge forward to engage the evil king in battle!"), nl, battle(king), !.
+
+event(_) :- !.
+
+
+
 /*
 edge(Start, Direction, Destination)
 Represents an edge from the current node, travelling in the given direction and reaching the specified end node. 
@@ -177,8 +171,7 @@ edge(tower, west, castle).
 edge(abandoned_house, east, crossroads).
 edge(abandoned_house, west, cemetery).
 edge(cemetery, east, abandoned_house).
-edge(cemetery, west, cemetery2).
-edge(cemetery2, east, cemetery).
+
 
 edge(armory, west, crossroads).
 edge(armory, south, slime_field).
@@ -189,21 +182,28 @@ edge(slime_field, east, cave) :- located(halberd, narnia), write("It would mean 
 edge(slime_field, east, cave).
 edge(cave, west, slime_field).
 
+
+map :- current_node_is(X), nl, format("currently in ~w. ", [X]), nl.
+map :- current_node_is(X), edge(X, north, N), nl, format("to the north is ~w.", [N]).
+map :- current_node_is(X), edge(X, west, N), nl, format("to the west is ~w.", [N]).
+map :- current_node_is(X), edge(X, south, N), nl, format("to the south is ~w.", [N]).
+map :- current_node_is(X), edge(X, east, N), nl, format("to the east is ~w.", [N]).  
 /*
 Initialize the location of the many items in the game.
 */
 
-located(short_sword, abandoned_house).
-located(chainmail, armory).
-located(halberd, cave).
-located(crown, boss).
-located(spellbook, tower).
-located(frost_wand, cemetery).
-located(magic_staff, forest).
-located(broadsword, shop).
-located(battle_axe, shop).
-located(breastplate, shop).
-located(fire_wand, shop).
+load_default_locations :-
+    assert(located(short_sword, abandoned_house)),
+    assert(located(chainmail, armory)),
+    assert(located(halberd, cave)),
+    assert(located(crown, boss)),
+    assert(located(spellbook, tower)),
+    assert(located(frost_wand, cemetery)),
+    assert(located(magic_staff, forest)),
+    assert(located(broadsword, shop)),
+    assert(located(battle_axe, shop)),
+    assert(located(breastplate, shop)),
+    assert(located(fire_wand, shop)).
 
 % add health potions to shop or somewhere else
 
@@ -227,6 +227,9 @@ item(nothing, weapon_slot, attack(player, 1)).
 item(nothing, armor_slot, defense(player, 1)).
 item(nothing, magic_slot, magic_attack(player, 1)).
 
+search :- current_node_is(X), nl, format("searching ~w for items. ", [X]).
+search :- current_node_is(X), located(I, X), nl, format("~w", [X]).
+search :- current_node_is(X), nl, write("found everything useful").
 /*
 Handles movement from node to node.
 */
@@ -244,11 +247,27 @@ w :- move(west).
 %f :- move(north).
 
 drink_health_potion:- current_node_is(X), different(battle_dimension, X), potion_count(health_potion, C), C > 0, heal(30), !.
+/*
+Initialize the iteractables of the game.
+*/
 
+interactable(coffin, cemetery).
+
+
+interact(X):- interactable(X, L), current_node_is(L), interaction_event(X), retract(interactable(X,L)).
+
+interaction_event(coffin):- status(ghost, alive), write("the coffin cover is cold and heavy. you notice the surrounding area is filled with cobwebs, it must have been closed for a while. with all your strength you bearly manage to open it."),nl,
+sleep(2), nl, write("A ghost emerges from the fog and attacks you!"), battle(ghost), !.
+
+
+
+uncover :- current_node_is(X), nl, format("searching ~w for intresting things. ", [X]).
+uncover :- current_node_is(X), interactable(I, X), nl, format("~w", [I]).
+uncover :- current_node_is(X), nl, write("found everything of interest.").
 /*
 Handles picking up items.
 */
-take(chest) :- assert(gold(50)), retract(gold(0)), assert(located(halberd, narnia)), retract(located(halberd, cave)), !.
+take(chest) :- add_gold(50), assert(located(halberd, narnia)), retract(located(halberd, cave)), !.
 take(I1) :-
     located(I1, shop), current_node_is(shop), item(I1, Y, S), equipped(Y, I2), different(item(I1, Y, S), item(_,potion,_)),
     buy(I1),
@@ -297,6 +316,7 @@ Describes the players gear.
 inspect(player) :-
     nl,
     gold(G),
+    potion_count(health_potion, P),
     equipped(head_slot, X),
     equipped(weapon_slot, Y),
     equipped(armor_slot, Z),
@@ -311,13 +331,12 @@ inspect(player) :-
     format("Weapon slot: ~w", [Y]), nl,
     format("Magic slot: ~w", [M]), nl,
     format("You currently have ~2f HP, ~w Attack, ~w Defence, ~w Magic Attack and ~w Magic Defence.", [H, A, D, MA, MD]), nl,
-    format("You are carrying ~w gold coins.", [G]), nl, !.
+    format("You are carrying ~w gold coins and ~w health potions.", [G, P]), nl, !.
 inspect(I1) :-
     located(I1, X), current_node_is(X), item(I1, Y, S1), equipped(Y, I2), item(I2, Y, S2), different(item(I1, Y, S), item(_,potion,_)),
     format("The item in your ~w has a value of ~w", [Y, S2]), nl,
     format("The item you see has a value of ~w", [S1]), nl, !.
 inspect(_) :- write("You can't inspect that!"), nl, !.
-
 
 /*
 Describes the current location to the player
@@ -365,7 +384,6 @@ description(armory) :-
 
 description(slime_field) :-
     located(halberd, cave),
-    gold(0),
     nl,
     write("This field has been infested with slimes since the spread of the king's chaos. You see a cave"), nl,
     write("to the east of the field's edge, or you can turn back north to the relative safety of the armory."), nl.
@@ -481,7 +499,19 @@ description(boss) :-
 Starts the game.
 */
 
-play :- tutorial, description(home).
+play :- assert(current_node_is(home)), options.
+
+new :- 
+    load_default_equipment, load_default_status, load_default_player_stats, load_default_locations,
+    tutorial, description(home).
+
+load :- load_game, retract(current_node_is(home)), !.
+
+options :-
+    nl,
+    write("Welcome to - The Legend of Lelda: Zink's Awakening!"), nl,
+    write("To begin you can either start a new game by typing 'new.'"), nl,
+    write("Or you can load a previous save by typing 'load.'"), nl, !.
 
 /*
 Basic instructions for players
@@ -489,12 +519,65 @@ Basic instructions for players
 
 tutorial :-
     nl,
-    write("Welcome to - The Legend of Lelda: Zink's Awakening!"), nl,
     write("You will recieve a short description of each location when you arrive there, pay attention to what's presented."), nl,
     write("You can move in four directions. North (n), East (e), South (s), and West (w). To move simply type the shorthand for a direction followed by a period."), nl,
     write("For example typing 'n.' would move you North."), nl,
     write("Once you reach a new location a short description of what you observe will be given."), nl,
     write("If you miss this description simply type 'look.' to have it be shown again."), nl,
+    write("to find all the paths you can take for an area and find your current location type 'map.'."), nl,
+    write("to find all interesting things to interact with type uncover."), nl,
+    write("to interact with things of interest type interact(X)."), nl,
+    write("to find all items in your current location type 'search.'."), nl,
     write("To pick up or equip an item type 'take(itemName)' where itemName can be any object the game describes such as a sword or armor."), nl,
     write("To inspect your current equipment type 'inspect(player).', to compare items type 'inspect(itemName).'"), nl,
     write("That's all for now. Enjoy the game!"), nl, !.
+
+/*
+Handles saving game state
+*/
+
+save :- current_node_is(battle_dimension), nl, write("Can't save during battles!"), nl, !.
+save :- save_unary_misc, save_binary_misc, save_equipped, save_status, save_located.
+
+save_unary_misc :- current_node_is(X), gold(Y), csv_write_file('save/save_unary_misc.txt', [current_node_is(X), gold(Y)]).
+
+save_binary_misc :- health(player, X), defense(player, Y), attack(player, Z), magic_attack(player, A), magic_defense(player, B), potion_count(health_potion, C),
+    csv_write_file('save/save_binary_misc.txt', 
+        [health(player, X), defense(player, Y), attack(player, Z), magic_attack(player, A), magic_defense(player, B), potion_count(health_potion, C)]).
+
+save_equipped :- findall(equipped(X, Y), equipped(X, Y), L), csv_write_file('save/save_equipped.txt', L).
+
+save_status :- findall(status(X, Y), status(X, Y), L), csv_write_file('save/save_status.txt', L).
+
+save_located :- findall(located(X,Y), located(X, Y), L), csv_write_file('save/save_located.txt', L).
+
+/*
+Handles loading game state
+*/
+
+load_game :- 
+    current_node_is(home), 
+    catch(see('save/save_unary_misc.txt'), E, (write('No save found'), fail_load)), 
+    load_unary_misc, load_binary_misc, load_equipped, load_status, load_located.
+load_game :- nl, write("Failed to load. Either no save files exist, or you already started your adventure."), nl, !.
+
+fail_load :- nl, play, fail.
+
+load_unary_misc :- csv_read_file('save/save_unary_misc.txt', X), load_unary_misc_state(X).
+load_unary_misc_state([row(X), row(Y)]) :- assert(current_node_is(X)), assert(gold(Y)).
+
+load_binary_misc :- csv_read_file('save/save_binary_misc.txt', X), load_binary_misc_state(X).
+load_binary_misc_state([row(X1, Y1), row(X2, Y2), row(X3, Y3), row(X4, Y4), row(X5, Y5), row(X6, Y6)]) :- 
+    assert(health(X1, Y1)), assert(defense(X2, Y2)), assert(attack(X3, Y3)), assert(magic_attack(X4, Y4)), assert(magic_defense(X5, Y5)), assert(potion_count(X6, Y6)).
+
+load_equipped :- csv_read_file('save/save_equipped.txt', X), load_equipped_state(X).
+load_equipped_state([row(X, Y)|T]) :- assert(equipped(X, Y)), load_equipped_state(T).
+load_equipped_state([]).
+
+load_status :- csv_read_file('save/save_status.txt', X), load_status_state(X).
+load_status_state([row(X, Y)|T]) :- assert(status(X, Y)), load_status_state(T).
+load_status_state([]).
+
+load_located :- csv_read_file('save/save_located.txt', X), load_located_state(X).
+load_located_state([row(X, Y)|T]) :- assert(located(X, Y)), load_located_state(T).
+load_located_state([]).
